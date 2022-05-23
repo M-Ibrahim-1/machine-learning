6fd83d8266583d8d6d2e58ac1ead44333c17539b
Muhammed Ibrahim <82919183+M-Ibrahim-1@users.noreply.github.com>
2022-05-23 16:54:27 +0200

    Gui created

--- Random_forest.py
+++ Random_forest.py
@@ -1,49 +1,48 @@
-#Importing the libraries
+# Importing the libraries
 
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
-#Importing the dataset
+import pickle
 
-#Training the Random Forest Regression model on the whole dataset
-#n_estimators = no of Trees
-
-def predict_from_meta_model():
-    pos = input("please enter the Postion: ")
-    vel = input("please enter the velocity: ")
-    angle = input("please enter the angle: ")
-    depth = input("please enter the depth: ")
-    width = input("please enter the width: ")
-    resultant_force = regressor.predict([[pos, vel, angle, depth, width]])
-    return resultant_force
+no_of_trees = 100
 
 
-def import_dataset():
-    dataset = pd.read_csv("dataset.csv")
-    dataset['res-force'] = dataset['res-force'].apply(np.ceil)
-    dataset.drop(['Unnamed: 0'], axis=1, inplace=True)
+class RandomForest():
+    def train():
+        dataset = pd.read_csv("dataset.csv")
+        dataset['res-force'] = dataset['res-force'].apply(np.ceil)
+        dataset.drop(['Unnamed: 0'], axis=1, inplace=True)
+        print(dataset)
+        x = dataset.iloc[:, :-1].values
+        print(x)
+        y = dataset.iloc[:, -1].values
+        x_train, x_test, y_train, y_test = train_test_split(x, y,
+                                                            test_size=0.2,
+                                                            random_state=1)
+        regressor = RandomForestRegressor(no_of_trees, random_state=1)
+        regressor.fit(x_train, y_train)
+        y_pred = regressor.predict(x_test)
+        np.set_printoptions(precision=2)
+        print(np.concatenate(
+            (y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)),
+            1))
+        # Evaluating the Model Performance
+        print("**************************")
+        print(f"R squared = {r2_score(y_test, y_pred)}")
+        filename = 'Random_forest_model.sav'
+        pickle.dump(regressor, open(filename, 'wb'))
+        return regressor
 
-    print(dataset)
-    x = dataset.iloc[:, :-1].values
-    print(x)
-    y = dataset.iloc[:, -1].values
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2,
-                                                        random_state=1)
-    return x_train, x_test, y_train, y_test
+    @staticmethod
+    def predict(position, velocity, angle, depth, width):
+        regressor = pickle.load(open('Random_forest_model.sav', 'rb'))
 
-
-regressor = RandomForestRegressor(n_estimators=500, random_state=1)
-x_train, x_test, y_train, y_test = import_dataset()
-
-regressor.fit(x_train, y_train)
-
-y_pred = regressor.predict(x_test)
-np.set_printoptions(precision=2)
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
-
-#Evaluating the Model Performance
-print("************")
-print(r2_score(y_test, y_pred))
+        resultant_force = regressor.predict([[position,
+                                              velocity,
+                                              angle,
+                                              depth,
+                                              width]])
+        return resultant_force
---
+++ main.py
@@ -0,0 +1,105 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from Random_forest import RandomForest
+from PyQt5.QtWidgets import QMessageBox
+import time
+
+class Ui_MainWindow(QtWidgets.QWidget):
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+
+        self.proceed = QtWidgets.QPushButton(self.centralwidget)
+        self.proceed.setGeometry(QtCore.QRect(160, 150, 93, 28))
+
+        # For displaying confirmation message along with user's info.
+        self.result_label = QtWidgets.QLabel(self.centralwidget)
+        self.result_label.setGeometry(QtCore.QRect(170, 40, 201, 111))
+        # Keeping the text of label empty initially.
+        self.result_label.setText("")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.proceed.setText(_translate("MainWindow", "Proceed"))
+        self.proceed.clicked.connect(self.take_inputs)
+
+
+    def take_inputs(self):
+
+        position, done1 = QtWidgets.QInputDialog.getInt(
+            self, 'Input Dialog', 'Enter Position:')
+
+        velocity, done2 = QtWidgets.QInputDialog.getDouble(
+            self, 'Input Dialog', 'Enter velocity:')
+
+        angle, done3 = QtWidgets.QInputDialog.getDouble(
+            self, 'Input Dialog', 'Enter angle:')
+
+        depth, done4 = QtWidgets.QInputDialog.getDouble(
+            self, 'Input Dialog', 'Enter depth:')
+
+        width, done5 = QtWidgets.QInputDialog.getInt(
+            self, 'Input Dialog', 'Enter width:')
+
+        if 0 < position <= 39 and 12 <= velocity <= 15 and 0 <= angle <= 3\
+                and 0 <= depth <= 4 and width in (1, 2, 3):
+            # Showing confirmation message along
+            # with information provided by user.
+            result = RandomForest.predict(position, velocity, angle, depth,
+                                          width)
+            print(result)
+            self.pop_resultant_force(result)
+            self.proceed.setText("Predict another set of Parameter")
+            self.proceed.adjustSize()
+
+            return position, velocity, angle, depth, width
+
+        else:
+            self.pop_err_parameter()
+            self.proceed.adjustSize()
+
+    def pop_resultant_force(self, result):
+        msg = QMessageBox()
+        msg.setWindowTitle("Result")
+        msg.setText(f'resultant force = {result}')
+        msg.setIcon(QMessageBox.Information)
+        x = msg.exec_()
+
+    @staticmethod
+    def pop_err_parameter():
+        msg = QMessageBox()
+        msg.setWindowTitle("Error")
+        msg.setText("Parameter Input was not Successful")
+        msg.setIcon(QMessageBox.Critical)
+        msg.setStandardButtons(QMessageBox.Retry | QMessageBox.Abort)
+        msg.setDetailedText("Position must be between 1 and 39 \n"
+                            "Velocity must be between 12 and 15 Km\H \n"
+                            "Angle must be between 0 and 3 Degree \n"
+                            "Depth must be between 0 and 4 mm \n"
+                            "Width can only be 1, 2 or 3")
+        x = msg.exec_()
+
+    @staticmethod
+    def pop_err_model_selection():
+        msg = QMessageBox()
+        msg.setWindowTitle("Error")
+        msg.setText("Please Select the Model before Proceeding...")
+        msg.setIcon(QMessageBox.Warning)
+        msg.setStandardButtons(QMessageBox.Retry | QMessageBox.Abort)
+        x = msg.exec_()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
